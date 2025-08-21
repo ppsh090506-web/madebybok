6fda584de99b38f299cc235e45fad4afe872aa3d
Bok <ppsh090506@gmail.com>
2025-08-21 20:28:49 +0900

    sign.lua

--- sing.lua
+++ sing.lua
@@ -13,7 +13,7 @@ local Window = Rayfield:CreateWindow({
       FileName = "Key",
       GrabKeyFromSite = true,
       SaveKey = true,
-      Key = {"https://pastebin.com/raw/R2kmUY1i"}
+      Key = {"VVIP"}
    }
 })
 
@@ -62,4 +62,5 @@ MainTab:CreateButton({
       loadstring(game:HttpGet("https://raw.githubusercontent.com/LKR-beta/nope/refs/heads/main/disabled"))()
    end
 })
+
 
